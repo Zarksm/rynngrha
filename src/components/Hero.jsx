db91478d5f9adeb339bf378fec293eda486851ae
zarksm <zarksm@Yanz.local>
2024-05-23 16:04:32 +0700

    fix bgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ const Hero = () => {
     <>
       <section className="w-full h-auto bg-primary">
         <div className="hero-container">
-          <h1 className="hero-title">Hi. I'm Zarksm</h1>
+          <h1 className="hero-title">Hi. I{"'"}m Zarksm</h1>
           <p className="hero-subtitle">
-            I'm passionate about crafting beautiful web design, that work great
-            on any device, and write clean tidy code.
+            I{"'"}m passionate about crafting beautiful web design, that work
+            great on any device, and write clean tidy code.
           </p>
         </div>
       </section>
