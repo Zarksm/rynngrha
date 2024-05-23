deefd3587ae6bdbc679a70ff6dd4b7fc5e8043ec
zarksm <zarksm@Yanz.local>
2024-05-23 21:33:07 +0700

    feat: adding lot of feature, style etc

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
         <section className="w-full h-auto md:h-auto">
           <div className="pt-36 md:pt-52 pb-5 md:pb-20">
             <h1 className="font-fairplay text-[46px] md:text-[80px] text-greenl text-center font-bold px-10 md:px-0">
-              I'm Riyan.
+              I{"'"}m Riyan.
             </h1>
           </div>
           <div className="w-full h-auto pb-10 md:pb-20 md:py-20 flex flex-col px-2 md:px-20 md:flex-row">
@@ -20,20 +20,20 @@ const AboutPage = () => {
             {/* right section */}
             <div className="md:w-3/5 md:ml-12 flex flex-col gap-5 pt-20 md:pt-0 px-5 md:px-0">
               <h1 className="font-bold font-fairplay text-greenl text-2xl md:text-4xl">
-                I'm a Frontend Dev working remotely from Bandung, Indonesia.
+                I{"'"}m a Frontend Dev working remotely from Bandung, Indonesia.
               </h1>
               <p className="font-sans text-white font-thin md:font-normal">
-                Over the past 2+ years, I've worked extensively in frontend
+                Over the past 2+ years, I{"'"}ve worked extensively in frontend
                 development, crafting user interfaces and experiences for web
                 applications. I have a solid foundation in HTML, CSS, and
                 JavaScript, along with experience working with modern frontend
                 frameworks like React, NextJS and Laravel.
               </p>
               <p className="font-sans text-white font-thin md:font-normal">
-                I'm actively expanding my skills by diving deeper into Next.js,
-                a powerful framework for building React applications. I'm
-                excited to leverage Next.js to develop fast, scalable, and
-                SEO-friendly web applications.
+                I{"'"}m actively expanding my skills by diving deeper into
+                Next.js, a powerful framework for building React applications. I
+                {"'"}m excited to leverage Next.js to develop fast, scalable,
+                and SEO-friendly web applications.
               </p>
             </div>
           </div>
