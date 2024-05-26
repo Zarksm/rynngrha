f1c0671d5047aec86bfe1f27f6007029b70af297
zarksm <zarksm@Yanz.local>
2024-05-26 17:54:06 +0700

    feat: anim, blog, etc

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div className="w-full h-auto bg-primary px-10 md:px-28 py-40">
-      <div className="py-10 container flex flex-col gap-20">
+    <div className="w-full h-auto bg-primary px-10 md:px-28 pt-32 md:pt-0 md:py-40">
+      <div className="py-10 md:container flex flex-col gap-10 md:gap-20">
         <div className="">
           <h1 className="font-fairplay text-[46px] md:text-[80px] text-greenl text-center font-bold px-10 md:px-0 mb-5 md:mb-0">
             Blogs
           </h1>
-          <p className="text-center text-greenls font-sans mt-10 text-xs md:text-lg md:w-2/3 md:mx-auto px-10 md:px-0">
+          <p className="text-center text-greenls font-sans mt-10 text-xs md:text-lg md:w-2/3 md:mx-auto">
             I write about web development, business, and personal growth. You
             can find my latest articles below.
           </p>
@@ -23,7 +23,7 @@ const Blog = () => {
               className="w-full bg-primary border border-greenls rounded-md overflow-hidden"
             >
               <Link href={`/blog/${blog.id}`}>
-                <div className="flex justify-between items-center p-10 hover:bg-greenls transition duration-300 ease-in-out">
+                <div className="flex flex-col-reverse md:flex-row justify-between gap-y-5 md:gap-y-0 md:items-center p-4 md:p-10 hover:bg-greenls transition duration-300 ease-in-out">
                   <div>
                     <h1 className="font-fairplay font-bold text-[20px] text-greenl">
                       {blog.title}
